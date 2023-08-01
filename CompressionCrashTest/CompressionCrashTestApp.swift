2e86c50866a496dfb9bf5d177b17d8a14d9231b8
Morten Just <morten.just@genstart.dk>
2023-08-01 12:51:00 +0200

    Initial Commit

---
+++ CompressionCrashTest/CompressionCrashTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CompressionCrashTestApp.swift
+//  CompressionCrashTest
+//
+//  Created by Morten Just on 8/1/23.
+//
+
+import SwiftUI
+
+@main
+struct CompressionCrashTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
